cc210ff89cbfa03735fe16275a769800ddb5f849
caner <canerserbest1997@gmail.com>
2022-01-12 23:56:19 +0300

    operatorler

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, c = 4;
+            bool sonuc = a > c;
+            Console.WriteLine(sonuc);
+            bool sonuc1 = a >= c;
+            Console.WriteLine(sonuc1);
+            bool sonuc2 = a < c;
+            Console.WriteLine(sonuc2);
+            Console.Read();
+        }
+    }
+}
